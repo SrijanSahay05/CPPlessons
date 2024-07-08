7defbf1731d1990036d63c1561c68bb64eb753d5
Srijan Sahay <srijan05sahay@gmail.com>
2024-07-08 12:43:04 +0530

    Functions Notes

--- lesson/functions.cpp
+++ lesson/functions.cpp
@@ -7,6 +7,10 @@ void sayHi() {
 }
 void add(int a, int b); //declaring a function up here but assigning values to function after int main()
 
+int sum(int num1, int num2){
+    int sum = num1 + num2; 
+    return sum;
+}
 double cube(double num){
     double result = num * num * num;
     return result; //return keyword in cpp breaks out of the function. so anything after the return keyword inside a defined function is not gonna execute
@@ -17,6 +21,7 @@ int main () {
     add(2,5);
     double ans = cube(5.12);
     cout << ans << endl;
+    cout << sum(3,5) << endl;
     return 0;
 }
 

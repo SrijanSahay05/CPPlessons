8df493c70722c4916a0780413443e42f72a28cbc
Srijan Sahay <srijan05sahay@gmail.com>
2024-07-09 09:50:13 +0530

    Max and Min values of array

--- questions/maxMinInArrays.cpp
+++ questions/maxMinInArrays.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <climits>
 
 using namespace std; 
 
 int main(){
     int n; 
+    int numMax = INT_MIN; 
+    int numMin = INT_MAX;
     int* numArray = new int[n]; //dynamic allocation is used if the size of the array has to be taken as input. (might be to avoid segmentation fault)
     cin >> n;
     for(int i=0; i<n; i++){
-        cout << "Enter the " << i+1 << " value of array" << endl;
         cin >> numArray[i];
     }
     for(int i=0; i<n; i++){
-        cout << numArray[i] << " "; 
+        if (numArray[i]>numMax){
+            numMax = numArray[i];
+        }
+        if (numArray[i]<numMin){
+            numMin = numArray[i];
+        }
     }
+    cout << "Max: " << numMax << endl;
+    cout << "Min: " << numMin << endl; 
     return 0;
 }
